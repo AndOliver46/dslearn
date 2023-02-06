a244658710d4c588da09c122ac487d724cea5bb2
AndOliver46 <anderson_souza75@live.com>
2023-02-06 14:29:04 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Topic;
+
+public interface TopicRepository extends JpaRepository<Topic, Long>{
+}
